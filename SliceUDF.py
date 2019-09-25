aa65f4f3db523a2b8353185f9bee5a9a6f006083
shashi72174 <shashi72174@gmail.com>
2019-09-25 09:05:15 +0530

    Add files via upload

--- SliceUDF.py
+++ SliceUDF.py
@@ -2,9 +2,25 @@ s="shashikiran"
 
 def slice(s,start,end,step):
     retStr = ""
-    for i in range(start,end,step):
-        retStr = retStr+s[i]
+    if step>0 and end<start:
+        return ""
+    elif step>0 and (end-start)<step:
+        return ""
+    elif step<0 and ((len(s)-end)+start)<0:
+        return ""
+
+    if step>0:
+        for i in range(start,end,step):
+            retStr = retStr+s[i]
+    else :
+        for i in range(start,(end-len(s)),step):
+            retStr = retStr + s[i]
+
 
     return retStr
 
-print(slice(s,2,6,2))
+#print(slice(s,10,11,2))
+#print(slice(s,5,2,1))
+#print(slice(s,1,5,4))
+print(slice(s,-2,3,-2))
+print(slice(s,2,3,-2))
